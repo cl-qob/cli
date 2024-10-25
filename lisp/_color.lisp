d6909b0036f9eefe7bb0a1f36d232e4005c7ed61
Jen-Chieh Shen <jcs090218@gmail.com>
2024-10-25 22:08:29 +0800

    wip status

--- lisp/_color.lisp
+++ lisp/_color.lisp
@@ -5,34 +5,57 @@
 (defvar qob-enable-color t
   "Set to nil to disable color.")
 
-(defun qob-color-code (color)
-  "Return the ANSI color code by COLOR."
+(defun qob-ansi-code (color)
+  "Return the ANSI code by COLOR."
   (ecase color
-    (:gray    "38;5;8")
-    (:black   30)
-    (:red     31)
-    (:green   32)
-    (:yellow  33)
-    (:blue    34)
-    (:magenta 35)
-    (:cyan    36)
-    (:white   37)))
+    ;; Style
+    (:bold       1)
+    (:dark       2)
+    (:italic     3)
+    (:underscore 4)
+    (:blink      5)
+    (:rapid      6)
+    (:contrary   7)
+    (:concealed  8)
+    (:strike     9)
+    ;; Color
+    (:gray      "38;5;8")
+    (:black     30)
+    (:red       31)
+    (:green     32)
+    (:yellow    33)
+    (:blue      34)
+    (:magenta   35)
+    (:cyan      36)
+    (:white     37)))
 
-(defun qob-color-it (color str)
-  "COLOR the STR."
-  (check-type color keyword)
+(defun qob-ansi-it (code str)
+  "Ansi the STR."
+  (check-type code keyword)
   (if qob-enable-color
       (format nil "~C[~Am~A~C[0m"
-              #\Esc (qob-color-code color) str #\Esc)
+              #\Esc (qob-ansi-code code) str #\Esc)
       str))
 
-(defun qob-ansi-black (str) (qob-color-it :black str))
-(defun qob-ansi-red (str) (qob-color-it :red str))
-(defun qob-ansi-green (str) (qob-color-it :green str))
-(defun qob-ansi-yellow (str) (qob-color-it :yellow str))
-(defun qob-ansi-blue (str) (qob-color-it :blue str))
-(defun qob-ansi-magenta (str) (qob-color-it :magenta str))
-(defun qob-ansi-cyan (str) (qob-color-it :cyan str))
-(defun qob-ansi-white (str) (qob-color-it :white str))
+;; Style
+(defun qob-ansi-bold (str) (qob-ansi-it :bold str))
+(defun qob-ansi-dark (str) (qob-ansi-it :dark str))
+(defun qob-ansi-italic (str) (qob-ansi-it :italic str))
+(defun qob-ansi-underscore (str) (qob-ansi-it :underscore str))
+(defun qob-ansi-blink (str) (qob-ansi-it :blink str))
+(defun qob-ansi-rapid (str) (qob-ansi-it :rapid str))
+(defun qob-ansi-contrary (str) (qob-ansi-it :contrary str))
+(defun qob-ansi-concealed (str) (qob-ansi-it :concealed str))
+(defun qob-ansi-strike (str) (qob-ansi-it :strike str))
+
+;; Color
+(defun qob-ansi-black (str) (qob-ansi-it :black str))
+(defun qob-ansi-red (str) (qob-ansi-it :red str))
+(defun qob-ansi-green (str) (qob-ansi-it :green str))
+(defun qob-ansi-yellow (str) (qob-ansi-it :yellow str))
+(defun qob-ansi-blue (str) (qob-ansi-it :blue str))
+(defun qob-ansi-magenta (str) (qob-ansi-it :magenta str))
+(defun qob-ansi-cyan (str) (qob-ansi-it :cyan str))
+(defun qob-ansi-white (str) (qob-ansi-it :white str))
 
 ;;; End of lisp/_color.lisp
--- lisp/core/status.lisp
+++ lisp/core/status.lisp
@@ -11,6 +11,37 @@
 
 (qob-init-asds)
 
-;; TODO: ..
+(defun qob-status--environment-name ()
+  "Get the working environment name."
+  (cond ((qob-global-p) "global (~~/)")
+        (t              "development (./)")))
+
+(defun qob-status--print-title (str)
+  "Print title STR."
+  (qob-println "")
+  (qob-println "~A" (qob-ansi-underscore str)))
+
+(defun qob-status--invocation ()
+  "Return the invocation path."
+  (cond
+    ((find-package :sb-ext) (truename (first sb-ext:*posix-argv*)))
+    ((find-package :ccl) (first (ccl:command-line-arguments)))
+    ((find-package :ext) (first ext:*argv*))
+    ((find-package :sys) (sys:executable-pathname))
+    (t (qob-error "Unsupported Lisp implementation."))))
+
+;;
+;;; Core
+
+(qob-println "In the ~A enviornment" (qob-status--environment-name))
+
+(qob-status--print-title "System:")
+(qob-println "   ~A: ~A" (lisp-implementation-type) (lisp-implementation-version))
+(qob-println "   Invocation: ~A" (qob-status--invocation))
+
+(when (qob-local-p)
+  (qob-status--print-title "Workspace:")
+  (dolist (name qob-loaded-asds)
+    (qob-println "   ~A" name)))
 
 ;;; End of lisp/core/status.lisp
