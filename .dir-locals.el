5855efd1589cc33ae5d2e7923e314f0aaaa2514c
Jen-Chieh Shen <jcs090218@gmail.com>
2024-11-02 01:42:41 +0800

    improve dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((lisp-mode . ((lsp-enabled-clients . nil)
-               (lsp-lisp-active-modes . nil))))
+((nil . ((lsp-disabled-clients . '(alive-lsp))
+         (lsp-lisp-active-modes . nil))))
